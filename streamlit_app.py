94dcca863f3df049a723a5207be0d5f1485e33f6
marthesheila <116018414+marthesheila@users.noreply.github.com>
2024-03-29 11:43:36 +0100

    Add dataframe and fruityvice_response in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -17,6 +19,10 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=true)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients'
