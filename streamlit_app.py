820802bb038ae58c9073e9116e875f3e37d801b4
marthesheila <116018414+marthesheila@users.noreply.github.com>
2024-03-29 16:21:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 
 # Write directly to the app
@@ -42,7 +43,7 @@ if ingredients_list:
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen +'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
